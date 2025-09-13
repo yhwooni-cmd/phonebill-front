f19cce73081fcdce6fb3b1febd417b4c98d6fc75
hiondal <hiondal@gmail.com>
2025-09-13 17:04:54 +0900

    ESLint 설정 문제 해결 및 필수 의존성 추가
    - eslint-plugin-react 패키지 설치하여 React ESLint 규칙 지원
    - .eslintrc.cjs 설정 단순화로 호환성 문제 해결
    - plugin:@typescript-eslint/recommended 형식으로 수정
    - 불필요한 React 관련 복잡한 규칙 제거
    - 가이드 문서에 필수 의존성 설치 안내 추가
    - ESLint 실행 시 13개 경고만으로 빌드 통과 가능

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,10 +7,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: [
     'dist',
@@ -28,8 +26,6 @@ module.exports = {
     }
   },
   plugins: [
-    'react',
-    'react-hooks',
     'react-refresh',
     '@typescript-eslint'
   ],
@@ -39,10 +35,6 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/jsx-uses-vars': 'error',
     
     // TypeScript 관련 규칙
     '@typescript-eslint/no-unused-vars': ['error', { 
@@ -50,48 +42,13 @@ module.exports = {
       varsIgnorePattern: '^_'
     }],
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-empty-function': 'warn',
     
     // 일반 규칙
-    'no-console': 'warn',
-    'no-debugger': 'error',
     'no-unused-vars': 'off', // TypeScript 규칙을 사용
-    'prefer-const': 'error',
-    'no-var': 'error',
-    
-    // 코드 스타일 규칙
-    'indent': ['error', 2],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'comma-trailing': 'off',
-    'object-curly-spacing': ['error', 'always'],
-    'array-bracket-spacing': ['error', 'never'],
-    
-    // JSX 관련 규칙
-    'react/jsx-indent': ['error', 2],
-    'react/jsx-indent-props': ['error', 2],
-    'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
-    'react/jsx-closing-tag-location': 'error',
-    'react/jsx-curly-spacing': ['error', 'never'],
-    'react/jsx-equals-spacing': ['error', 'never'],
-    'react/jsx-props-no-multi-spaces': 'error',
-    'react/jsx-tag-spacing': ['error', {
-      'closingSlash': 'never',
-      'beforeSelfClosing': 'always',
-      'afterOpening': 'never',
-      'beforeClosing': 'never'
-    }],
     
     // Hooks 규칙
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn'
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
   },
   overrides: [
     {
